44ae97bf6f68ecef735f5d6a3f0b7081e0630455
PaoloBarahona <20240048@ricaldone.edu.sv>
2025-07-22 21:19:46 -0600

    arreglo

--- src/main/java/BuSmart/APIBuSmart/Service/UserService.java
+++ src/main/java/BuSmart/APIBuSmart/Service/UserService.java
@@ -5,8 +5,6 @@ import BuSmart.APIBuSmart.Exceptions.ExcepUsuarios.ExceptionsUsuarioNoEncontrado
 import BuSmart.APIBuSmart.Models.DTO.UserDTO;
 import BuSmart.APIBuSmart.Repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
