703982b4a6b84813dd666dfde637c01cf3dbafdd
PaoloBarahona <20240048@ricaldone.edu.sv>
2025-07-22 21:21:03 -0600

    Se arregla

--- src/main/java/BuSmart/APIBuSmart/Controllers/UserController.java
+++ src/main/java/BuSmart/APIBuSmart/Controllers/UserController.java
@@ -11,9 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import javax.naming.Binding;
-import javax.xml.stream.events.DTD;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
